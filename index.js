ee19ea7f03723befc07318a2cb4a376703796d66
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-07-03 20:55:44 +0700

    Add getopt CLI args

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const genPageData = require('./gen-page-data')
+const Getopt = require('node-getopt')
 
-console.log(genPageData())
+const convert = require('./converter')
+
+const CLI_ARGS = [
+  ['o', 'output=ARG', 'name of output file (default: stdout)'],
+  ['i', 'input=ARG', 'name of input CSV file (default: stdin)'],
+  ['v', 'maxVisits=ARG', 'max number of visit docs to generate per page (default: 10)'],
+  ['h', 'help', 'display this help'],
+]
+
+const args = new Getopt(CLI_ARGS)
+args.bindHelp()
+
+const { options } = args.parseSystem()
+
+convert(options)
